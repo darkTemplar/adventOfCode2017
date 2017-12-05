36e7af6257791675b3045f30faad0353980799d7
Abhas <abhasforyou@gmail.com>
2017-12-05 09:36:50 -0800

    Complete day2 part ii)

--- day2.exs
+++ day2.exs
@@ -3,14 +3,25 @@ defmodule Checksum do
 	@doc """
 	calculate checksum of a 2d array by subtracting max and min from each row and then summing it up
 	"""
-	def checksum(filename) do
+	def checksum1(filename) do
 		filename
-		|> parseInput
+		|> parse_input
 		|> Enum.map(fn(lst) -> Enum.max(lst) - Enum.min(lst) end)
 		|> Enum.sum
 	end
 
-	defp parseInput(filename) do
+	def checksum2(filename) do
+		filename
+		|> parse_input
+		|> Enum.map(&Checksum.coefficient_evenly_divisible_pair/1)
+		|> Enum.sum
+	end
+
+	def coefficient_evenly_divisible_pair(lst) do
+		hd(for i <- lst, j <- lst, i > j && rem(i, j) == 0, do: div(i, j))
+	end
+
+	def parse_input(filename) do
 		File.read!(filename)
 		|> String.trim
 		|> String.split("\n")
